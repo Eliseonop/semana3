bc69e10bb190a7332365f4876c003e78e0a8a36d
Eliseo Falcon <eliseonop@gmail.com>
2022-10-31 16:19:33 -0500

    update

--- caso2.js
+++
@@ -1,6 +0,0 @@
-for (let i = 1; i <= 10; i++) {
-  // mostrar 1 circulo y que desplace de arriba hacia abajo
-  document.body.innerHTML += `<marquee direction="down" behavior="alternate" SCROLLAMOUNT="5"  > 
-    <div id="circulo" ></div>
-            </marquee>`
-}
