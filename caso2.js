6f262fff5d483353b447df49f084315b0973bfe2
Eliseo Falcon <eliseonop@gmail.com>
2022-10-31 15:49:00 -0500

    update: cambios

---
+++ caso2.js
@@ -0,0 +1,3 @@
+for(let i=1;i<=10;i++){
+    document.body.innerHTML += `<marquee direction="left" behavior="alternate" SCROLLAMOUNT="5" height="600" > <div id="circulo" ></div> </marquee>`;
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+for(let i=1; i<=10; i++) {
+    document.body.innerHTML += `<marquee direction="left" behavior="alternate" SCROLLAMOUNT="5"  ">
+    <div id="circulo" ></div>
+    </marquee>`;
+}
